98231c050341e12fc1520e9d688d6f1b569f67b2
Rajani087 <40889469+Rajani087@users.noreply.github.com>
2018-07-17 17:25:59 +0530

    Add files via upload
    angular material

---
+++ Angular material/Day1/app.js
@@ -0,0 +1,12 @@
+angular.module("abc",['ngMaterial', 'ngMessages'])
+.controller("mat",mat)
+function mat(){
+    var abc=this;
+    abc.raj="mech";
+    abc.value=function()
+    {
+        
+    }
+
+}
+
---
+++ Angular material/app.js
@@ -0,0 +1,6 @@
+angular.module("abc",['ngMaterial', 'ngMessages'])
+.controller("mat",mat)
+function mat(){
+    
+}
+
